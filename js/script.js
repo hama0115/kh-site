b58a7a82123bc8b2d112ec6b44f10c38dd48b206
hama0115 <oh.akibee1231@gmail.com>
2025-05-13 06:45:33 +0900

    画像をクリックすることで音楽の再生、停止ができるようにしてみた

--- js/script.js
+++ js/script.js
@@ -11,3 +11,27 @@ for (let i = 0; i < singleItems.length; i++ ) {
   };
   singleItems[i].animate(keyframes, options);
 }
+
+const imageAreas = document.querySelectorAll('.image-area');
+
+imageAreas.forEach(imageArea => {
+  const audio = imageArea.querySelector('.audio-player');
+  const icon = imageArea.querySelector('.play-icon');
+
+  imageArea.addEventListener('click', () => {
+    if (audio.paused) {
+      audio.play();
+      icon.classList.remove('fa-circle-play');
+      icon.classList.add('fa-circle-pause');
+    } else {
+      audio.pause();
+      icon.classList.remove('fa-circle-pause');
+      icon.classList.add('fa-circle-play');
+    }
+  });
+
+  audio.addEventListener('ended', () => {
+    icon.classList.remove('fa-circle-pause');
+    icon.classList.add('fa-circle-play');
+  });
+});
